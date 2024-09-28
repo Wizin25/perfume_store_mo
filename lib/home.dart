80f51e941e999fce66590a0f3e7a45798f5039e4
phuthse <phuthse85@gmail.com>
2024-09-28 15:22:36 +0700

    fix color

--- lib/home.dart
+++ lib/home.dart
@@ -4,7 +4,7 @@ class Home  extends StatefulWidget {
   const Home ({super.key});
 
   @override
-  State< Home > createState() => _HomeState();
+  State < Home > createState() => _HomeState();
 }
 
 class _HomeState extends State< Home > {
@@ -13,21 +13,21 @@ class _HomeState extends State< Home > {
 
   List<Widget> screens = [
     Container(
-      color: const Color.fromARGB(255, 245, 145, 188),
+      color: Colors.grey,
       child: const Text("Home Screen Widget"),
     ),
 
     Container(
-      color: const Color.fromARGB(255, 245, 145, 188),
+      color: Colors.grey,
       child: const Text("Activity Screen Widget"),
     ),
 
     Container(
-      color: Colors.white,
+      color: Colors.grey,
       child: const Text("Cart Screen Widget"),
     ),
     Container(
-      color: Colors.white,
+      color: Colors.grey,
       child: const Text("Profile Screen Widget"),
     )
   ];
@@ -42,20 +42,38 @@ class _HomeState extends State< Home > {
   Widget build(BuildContext context) {
     return Scaffold( 
       appBar: AppBar(
-        title: const Text("Perfume Store"),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 190, 193, 196),
+        title: const Text (
+          "Perfume Store",
+          style: const TextStyle(
+            fontSize: 20.0,
+          ),
+        ),
+        leading: const Icon(Icons.menu),
+
+        actions:<Widget> [
+          IconButton(onPressed: (){
+            //Do something here
+          }, icon: const Icon(Icons.search)),
+        ],
       ),
+      
       bottomNavigationBar: BottomNavigationBar(
-        unselectedItemColor: Colors.white,
+        
+        unselectedItemColor: Colors.grey,
         selectedItemColor: Colors.black,
         currentIndex: currentIndex,
         onTap: _onItemTapped,
-        items: const [
+        
+        items: const [   
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.message), label: "Activity"),
           BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: "Cart"),
           BottomNavigationBarItem(icon: Icon(Icons.manage_accounts), label: "Profile"),
-        ]
-         ),
+        ],
+
+      ),
       body: screens.elementAt(currentIndex), 
     
     );
